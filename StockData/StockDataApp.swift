836b8b9ebf860c4e6c3c9ef01c660de7dde293fc
Matthew Ramsden <ramsden@hey.com>
2021-12-14 14:33:02 -0600

    first commit

---
+++ StockData/StockDataApp.swift
@@ -0,0 +1,19 @@
+//
+//  StockDataApp.swift
+//  StockData
+//
+//  Created by Matthew Ramsden on 12/14/21.
+//
+
+import SwiftUI
+// https://stock-data-yahoo-finance-alternative.p.rapidapi.com/v6/finance/quote?symbols=AAPL,GOOG,SQ,ETH
+// https://rapidapi.com/principalapis/api/stock-data-yahoo-finance-alternative/
+
+@main
+struct StockDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
