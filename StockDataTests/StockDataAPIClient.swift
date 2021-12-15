dcb693a76875c95ad5ea8b48f159b9e448c3ccdf
Matthew Ramsden <ramsden@hey.com>
2021-12-15 16:50:11 -0600

    tests: apiclient test

---
+++ StockDataTests/StockDataAPIClient.swift
@@ -0,0 +1,43 @@
+//
+//  StockDataAPIClient.swift
+//  StockDataTests
+//
+//  Created by Matthew Ramsden on 12/15/21.
+//
+
+import XCTest
+@testable import StockData
+
+class StockDataAPIClient: XCTestCase {
+
+//    override func setUpWithError() throws {
+//        // Put setup code here. This method is called before the invocation of each test method in the class.
+//    }
+//
+//    override func tearDownWithError() throws {
+//        // Put teardown code here. This method is called after the invocation of each test method in the class.
+//    }
+//
+//    func testExample() throws {
+//        // This is an example of a functional test case.
+//        // Use XCTAssert and related functions to verify your tests produce the correct results.
+//        // Any test you write for XCTest can be annotated as throws and async.
+//        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+//        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+//    }
+//
+//    func testPerformanceExample() throws {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
+    
+    func testgenerateNewsURL() {
+        let url = generateNewsURL()
+        // hide key or somethin
+        let fakeURL = "https://stock-data-yahoo-finance-alternative.p.rapidapi.com/v6/finance/quote?symbols=AAPL,LMND,SQ"
+        XCTAssertEqual(url, URL(string: fakeURL))
+    }
+
+}
