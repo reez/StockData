0239dfa432803a4958def61c5fe2d1f56bf14dfc
Matthew <6657488+reez@users.noreply.github.com>
2021-12-16 15:38:57 -0600

    cache: disk
    * cache: stub out cache type and implementation
    
    * cache: implement cache in viewmodel and view

--- StockData/ResponseViewModel.swift
+++ StockData/ResponseViewModel.swift
@@ -13,28 +13,48 @@ enum DataFetchPhase<T> {
     case failure(Error)
 }
 
+struct FetchTaskToken: Equatable {
+//    var category: Category
+    var token: Date
+}
+
 class ResponseViewModel: ObservableObject {
     @Published var phase = DataFetchPhase<[Stock]>.empty
     @Published var stocks: [Stock]? = nil
-    
+    @Published var fetchTaskToken: FetchTaskToken = FetchTaskToken(token: Date())
+
     let fetch: () async throws -> [Stock]
-    
+    private let cache = DiskCache<[Stock]>(filename: "stockdata_articles", expirationInterval: 3 * 60)
+
     init(fetch: @escaping () async throws -> [Stock]) {
         self.fetch = fetch
+        Task(priority: .userInitiated) {
+             try? await cache.loadFromDisk()
+         }
     }
     
     func refreshTask() async {
-        //fetchTaskToken.token = Date()
-        await loadArticles()
+        fetchTaskToken.token = Date()
+        await cache.removeValue(forKey: "category")
+//        await loadArticles()
     }
     
     @MainActor
     func loadArticles() async {
         self.stocks = nil
         if Task.isCancelled { return }
+        if let articles = await cache.value(forKey: "category") { // hardcode category because i dont need
+              phase = .success(articles)
+              print("CACHE HIT")
+            return
+          }
+        print("CACHE MISSED/EXPIRED")
         do {
             let articles = try await self.fetch()
             if Task.isCancelled { return }
+            await cache.setValue(articles, forKey: "category")
+            try? await cache.saveToDisk()
+            print("CACHE SET")
             phase = .success(articles)
             self.stocks = articles
         } catch {
